e4b66fd5435acac3ad54de6f296cdcf079c82d67
perpetua <onaperpetua@gmail.com>
2022-04-08 01:25:14 -0700

    commit messgae

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - entry point
-*
-*Return: 0 after printing the funtion
-*/
+ *main - entry point
+ *Return: 0 after printing
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
