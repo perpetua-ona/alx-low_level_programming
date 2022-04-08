9d307912d99125de78dd5e6ff00da0e7022c61de
perpetua <onaperpetua@gmail.com>
2022-04-08 01:44:59 -0700

    commit for printn funtions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+prinf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
