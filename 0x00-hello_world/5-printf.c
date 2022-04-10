f99d8f55a241e1c660ba9747331a3bdc4768b67c
perpetua <onaperpetua@gmail.com>
2022-04-10 10:02:36 -0700

    my commit for this program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ *main - Entry poin
  *My hello, printf task
- *Return: return 1 after program execution
+ *Return: return 1 after program execute
  */
 int main(void)
 {
-prinf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the output is a piece of art,\n");
 return (0);
 }
