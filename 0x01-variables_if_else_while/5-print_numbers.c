54a5ee900962c8ace0d8a8a5b6c9bef58050f75a
perpetua <onaperpetua@gmail.com>
2022-04-11 20:45:23 -0700

    my commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+int numbers;
+
+for (numbers = 0 ; numbers < 10; numbers++)
+{
+printf("%numbers", numbers);
+}
+
+printf("\n");
+
+return (0);
+
+}
