33dc15bf673781511df80a2a24421888f401a880
perpetua <onaperpetua@gmail.com>
2022-04-11 22:53:38 -0700

    commit for the taskk

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int digits;
+
+for (digits = 48; digits <= 57; digits++)
+{
+putchar(digits);
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
