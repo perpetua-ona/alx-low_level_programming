aa604f9a55884604488fab5e1aff395447355466
perpetua <onaperpetua@gmail.com>
2022-04-11 21:10:26 -0700

    commiting again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int mai (void)
+
+{
+int d;
+
+for (d = 48 ; d <= 57; d++)
+{
+putchar (d);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
