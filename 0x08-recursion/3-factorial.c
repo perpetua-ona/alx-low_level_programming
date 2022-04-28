a32e4e35c89085f0294b384c5db72f717027860e
perpetua <onaperpetua@gmail.com>
2022-04-28 12:12:16 -0700

    commit message

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * factorial - function to return the factorial of n
+ * @n: parameter
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+
+	return (0);
+}
+
